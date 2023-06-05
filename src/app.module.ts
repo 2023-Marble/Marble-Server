4f17f7db9151d4c884e4f0e45412d95df8f01cd0
Minji Kim <sml07113@gmail.com>
2023-06-05 23:46:36 +0900

    feat: dotenv 설치 및 docker-compose 파일 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,16 @@ import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { MosaicController } from './mosaic/mosaic.controller';
 import { ImageController } from './image/image.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UserController, MosaicController, ImageController],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  controllers: [
+    AppController,
+    UserController,
+    MosaicController,
+    ImageController,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
