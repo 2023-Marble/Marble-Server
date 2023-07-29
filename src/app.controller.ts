88e76dd33ca8889a4eb93d41251e7c0d1213c1e0
Minji Kim <sml07113@gmail.com>
2023-07-30 01:29:44 +0900

    del: 불필요한 코드 및 파일 삭제

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Mosaic } from './mosaic/schemas/mosaic.schema';
 
 @Controller()
 export class AppController {
@@ -9,10 +8,5 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @Get('/test')
-  findAll(): Promise<Mosaic[]> {
-    return this.appService.findAll();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,20 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Mosaic } from './mosaic/schemas/mosaic.schema';
-import { Repository } from 'typeorm';
 
 @Injectable()
 export class AppService {
-  constructor(
-    @InjectRepository(Mosaic)
-    private mosaicRepository: Repository<Mosaic>,
-  ) {}
+  constructor() {}
 
   getHello(): string {
     return 'Marble';
-  }
-
-  findAll(): Promise<Mosaic[]> {
-    return this.mosaicRepository.find();
   }
 }
